0985a82fcee79e6ad783b5d3f81dc812c3143a32
CaidevOficial <falcone.facu@gmail.com>
2022-06-05 20:22:46 -0300

    Upd :gear: - Functions

--- 05_Functions/Functions.sql
+++ 05_Functions/Functions.sql
@@ -1,30 +1,104 @@
-DELIMITER // 
-CREATE FUNCTION add_days(actual_date DATE, amount_days INT) 
-RETURNS DATE 
-BEGIN 
-RETURN actual_date + INTERVAL amount_days DAY;
-END // 
-CREATE FUNCTION get_amount_pages() 
-RETURNS INT 
-BEGIN
+-- Initialize the variable
+SELECT
+  @now = CURDATE();
+
+SELECT
+  SECOND(@now) as seconds,
+  MINUTE(@now) as minutes,
+  HOUR(@now) as hours,
+  DAY(@now) as day,
+  MONTH(@now) as month,
+  YEAR(@now) as year;
+
+SELECT
+  DAYOFWEEK(@now) as day_of_week,
+  DAYOFMONTH(@now) as day_of_month,
+  DAYOFYEAR(@now) as day_of_year,
+  WEEKOFYEAR(@now) as week_of_year,
+  QUARTER(@now) as quarter;
+
+SELECT
+  DATE(@now) as timestamp_to_date;
+
+-- Returns all the books that have been created today.
+SELECT
+  *
+FROM
+  `books` AS b
+WHERE
+  DATE(b.publish_date) = CURDATE();
+
+-- Adds 30 days to the current date
+-- The type of the interval could be SECOND, MINUTE, HOUR, DAY, WEEK, MONTH, or YEAR.
+SELECT
+  @now + INTERVAL 30 DAY;
+
+-- Subtracts 30 days from the current date
+-- The type of the interval could be SECOND, MINUTE, HOUR, DAY, WEEK, MONTH, or YEAR.
+SELECT
+  @now - INTERVAL 30 DAY;
+
+-- Returns a value 'Yes' if the condition is true, otherwise returns 'No'
+SELECT
+  IF(@now > DATE('2020-01-01'), 'Yes', 'No') as is_after_2020;
+
+-- Returns a value bassed on the amount of pages of the books
+SELECT
+  IF(b.pages > 500, 'Long book', 'Short book') as long_or_short_book
+FROM
+  `books` AS b;
+
+-- Returns a value to evaluate if is null or not.
+SELECT
+  IFNULL(b.pages, 'No pages') as pages_or_no_pages
+FROM
+  `books` AS b;
+
+-- Creating Functions
+-- Specifies an specific delimitator for the function
+-- You can use any symbol to delimit the function, for example //
+DELIMITER / / CREATE FUNCTION add_days(actual_date DATE, amount_days INT) RETURNS DATE BEGIN RETURN actual_date + INTERVAL amount_days DAY;
+
+END / / CREATE FUNCTION get_amount_pages() RETURNS INT BEGIN
 SET
   @pages = (
     SELECT
       (ROUND(RAND() * 100) * 4)
   );
+
 RETURN @pages;
-END //
-CREATE FUNCTION get_amount_sales()
-RETURNS INT
-BEGIN
+
+END / / CREATE FUNCTION get_amount_sales() RETURNS INT BEGIN
 SET
   @pages = (
     SELECT
       (ROUND(RAND() * 100) * 6)
   );
-RETURN @pages;
-END // DELIMITER;
 
+RETURN @pages;
+
+END / / DELIMITER;
+
+-- To use the functions you need to specify the default delimiter ;
+DELIMITER;
+
+-- To list all the functions that you have created in the database
+-- The table proc is a special table that contains all the functions & procedures
+SELECT
+  f.name
+FROM
+  mysql.proc AS f
+WHERE
+  f.db = DATABASE()
+  AND f.type = 'FUNCTION';
+
+-- To list all the functions you can use the following command:
+SHOW FUNCTION STATUS;
+
+-- To drop a function that you have created use drop
+DROP FUNCTION add_days;
+
+-- Update a field using a function
 UPDATE
   libros
 SET
--- 05_Functions/Statements.sql
+++ 05_Functions/Statements.sql
@@ -1,114 +1,47 @@
-DROP DATABASE IF EXISTS library_ff;
-CREATE DATABASE IF NOT EXISTS library_ff;
 USE library_ff;
 
-CREATE TABLE IF NOT EXISTS `authors`(
-  `author_id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(25) NOT NULL,
-  `surname` VARCHAR(25) NOT NULL,
-  `alias` VARCHAR(50) UNIQUE,
-  `gender` ENUM('M', 'F'),
-  `birthday` DATE NOT NULL,
-  `origin_country` VARCHAR(40) NOT NULL,
-  `create_date` DATETIME DEFAULT current_timestamp
-);
+-- Concat many strings into a single string
+SELECT CONCAT(a.name, ' ', a.surname) AS full_name
+FROM `authors` as a;
 
-CREATE TABLE `books`(
-  `book_id` INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-  `author_id` INT UNSIGNED NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `description` varchar(250) NOT NULL DEFAULT '',
-  `amount_pages` INTEGER UNSIGNED NOT NULL DEFAULT 0,
-  `publish_date` Date NOT NUll,
-  `create_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (author_id) REFERENCES authors(author_id)
-);
+-- Get the length of a string
+SELECT LENGTH(a.name) AS name_length
+FROM `authors` as a;
 
-ALTER TABLE
-  `books`
-ADD
-  sales INT UNSIGNED NOT NULL DEFAULT 0;
+-- Gets rows where the name is smaller than 7 characters
+SELECT * FROM `authors` AS a
+WHERE LENGTH(a.name) > 7;
 
-ALTER TABLE
-  `books`
-ADD
-  stock INT UNSIGNED DEFAULT 10;
+-- Converts the string to uppercase or lowercase
+SELECT UPPER(a.name) AS name_upper, LOWER(a.surname) AS surname_lower
+FROM `authors` as a;
 
-INSERT INTO `authors` (name, surname, alias, birthday, gender, origin_country )
-VALUES
-('Stephen Edwin', 'King', 'Richard Bachman', '1947-09-27', 'M', 'USA'),
-('Joanne', 'Rowling', 'J.K Rowling', '1947-09-27', 'F', 'Reino unido'),
-('Daniel', 'Brown',  NULL, '1964-06-22', 'M', 'USA'),
-('John', 'Katzenbach ', NULL,'1950-06-23', 'M', 'USA'),
-('John Ronald', 'Reuel Tolkien', NULL, '1892-01-03', 'M', 'Reino unido'),
-('Miguel', 'de Unamuno', NULL, '1892-01-03', 'M', 'USA'),
-('Arturo', 'Pérez Reverte', NULL, '1951-11-25', 'M', 'España'),
-('George Raymond', 'Richard Martin', NULL, '1948-09-20', 'M', 'USA');
+-- Removes the spaces at the beginning and end of a string
+SELECT TRIM(a.name) AS name_trim
+FROM `authors` as a;
 
-INSERT INTO `books`(author_id, title, publish_date)
-VALUES 
-(1, 'Carrie','1974-01-01'),
-(1, 'El misterio de Salmes Lot','1975-01-01'),
-(1, 'El resplando','1977-01-01'),
-(1, 'Rabia','1977-01-01'),
-(1, 'El umbral de la noche','1978-01-01'),
-(1, 'La danza de la muerte','1978-01-01'),
-(1, 'La larga marcha','1979-01-01'),
-(1, 'La zona muerta','1979-01-01'),
-(1, 'Ojos de fuego','1980-01-01'),
-(1, 'Cujo','1981-01-01'),
-(1, 'La torre oscura 1 El pistolero','1982-01-01'),
-(1, 'La torre oscura 2 La invocación','1987-01-01'),
-(1, 'Apocalipsis','1990-01-01'),
-(1, 'La torre oscura 3 Las tierras baldías','1991-01-01'),
-(1, 'La torre oscura 4 Bola de cristal','1997-01-01'),
-(1, 'La torre oscura 5 Los de Calla','2003-01-01'),
-(1, 'La torre oscura 6 La torre oscura','2004-01-01'),
-(1, 'La torre oscura 7 Canción de Susannah','2004-01-01'),
-(1, 'La niebla','1981-01-01'),
+-- Creates a substring of size 3 [the first 3 characters] from the start of the string
+SELECT LEFT(a.name, 3) AS name_left
+FROM `authors` as a;
 
-(2, 'Harry Potter y la Piedra Filosofal', '1997-06-30'),
-(2, 'Harry Potter y la Cámara Secreta', '1998-07-2'),
-(2, 'Harry Potter y el Prisionero de Azkaban','1999-07-8'),
-(2, 'Harry Potter y el Cáliz de Fuego','2000-03-20'),
-(2, 'Harry Potter y la Orden del Fénix','2003-06-21'),
-(2, 'Harry Potter y el Misterio del Príncipe','2005-06-16'),
-(2, 'Harry Potter y las Reliquias de la Muerte','2007-07-21'),
+-- Creates a substring of size 3 [The last 3 characters] from the end of the string
+SELECT RIGHT(a.name, 3) AS name_right
+FROM `authors` as a;
 
-(3, 'Origen', '2017-01-01'),
-(3, 'Inferno', '2013-01-01'),
-(3, 'El simbolo perdido', '2009-01-01'),
-(3, 'El código Da Vinci', '2006-01-01'),
-(3, 'La consipiración', '2003-01-01'),
+-- Returns all the rows where the name starts with the 'HARRY POTTER'
+SELECT * FROM `books` AS b
+WHERE LEFT(UPPER(b.title), 12) = 'HARRY POTTER';
 
-(4, 'Al calor del verano', '1982-01-01'),
-(4, 'Un asunto pendiente', '1987-01-01'),
-(4, 'Juicio Final', '1992-01-01'),
-(4, 'La sombra', '1995-01-01'),
-(4, 'Juego de ingenios', '1997-01-01'),
-(4, 'El psicoanalista', '2002-01-01'),
-(4, 'La historia del loco', '2004-01-01'),
-(4, 'El hombre equivocado', '2006-01-01'),
-(4, 'El estudiante', '2014-01-01'),
+-- Returns all the rows where the title of the books ends with 'ANILLOS'
+SELECT * FROM `books` AS b
+WHERE RIGHT(UPPER(b.title), 7) = 'ANILLOS';
 
-(5, 'El hobbit','1937-01-01'),
-(5, 'Las dos torres','1954-01-01'),
-(5, 'El señor de los anillos','1954-01-01'),
-(5, 'La comunidad del anillo','1954-01-01'),
-(5, 'El retorno del rey','1955-01-01'),
+-- Returns a random number between 0 and 100 rouded to the nearest integer
+SELECT ROUND(RAND()*100) AS random_number;
 
-(6, 'La niebla','1914-01-01'),
+-- Returns a truncate decimal number to a certain number of decimal places
+SELECT TRUNCATE(RAND()*100, 2) AS truncate_random_number;
 
-(7, 'Eva','2017-01-01'),
-(7, 'Falcó','2016-01-01'),
-(7, 'Hombre buenos','2015-01-01'),
-(7, 'Los barcos se pierden en tierra','2011-01-01'),
+-- Returns a number powered by another number
+SELECT POWER(RAND()*100, 2) AS power_random_number;
 
-(8, 'Juego de tronos','1996-08-01'),
-(8, 'Choque de reyes','1998-11-16'),
-(8, 'Tormenta de espadas','2005-10-17'),
-(8, 'Festin de cuervos','2011-07-12'),
-(8, 'Danza de dragones','2011-07-12');
-
-DESC autores;
-DESC libros;
